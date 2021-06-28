9e475ebad4196c967eba60300f0578f8c52e3aaa
manishjaingit <manishjain1706@gmail.com>
2021-06-28 18:38:36 +0530

    user-1

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -13,7 +13,7 @@ export class TutorialService {
     return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=munich&appid=252251328f6829ec98f890de1f08cd7f&lang=en');
   }
   getHistoricWeather(){
-    return this.http.get('http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid=1e536f45c13de7c930899cf4a6b96bd6');
+    return this.http.get('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1624885513&appid=252251328f6829ec98f890de1f08cd7f ');
 
   }
   
